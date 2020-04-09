a0adfdc722c6c8855e65489589307b2c99341d86
Jhonatan Sepulveda <jsepulveda@todo1.net>
2020-04-09 17:58:31 -0500

    Complete CRUD for patients

--- src/main/java/com/cidenet/hospital/controller/PatientController.java
+++ src/main/java/com/cidenet/hospital/controller/PatientController.java
@@ -3,14 +3,17 @@ package com.cidenet.hospital.controller;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -75,5 +78,36 @@ public class PatientController {
 		
 			return response;
 		}
+	}
+	
+	/**
+	 * Get a patient with a specific id
+	 * @param patientId
+	 * @return a patient
+	 */
+	@GetMapping("patient/{id}")
+	public ResponseEntity<Optional<Patient>> getPatientById(@PathVariable(value = "id") Long patientId) {
+		Optional<Patient> patient = patientRepository.findById(patientId);
+		return ResponseEntity.ok().body(patient);
+	}
+	
+	/**
+	 * Update patient
+	 * @param patientId
+	 * @param patientDetails
+	 * @return patient updated
+	 */
+	@PutMapping("patient/{id}")
+	public ResponseEntity<Patient> updatePatient(@PathVariable(value = "id") Long patientId, @Valid @RequestBody Patient patientDetails) {
+		Patient patient = patientRepository.findById(patientId).get();
+		
+		patient.setId(patientDetails.getId());
+		patient.setFirstName(patientDetails.getFirstName());
+		patient.setLastName(patientDetails.getLastName());
+		patient.setEmail(patientDetails.getEmail());
+		patient.setPhone(patientDetails.getPhone());
+		Patient updatedPatient = patientRepository.save(patient);
+		
+		return ResponseEntity.ok().body(updatedPatient);
 	}
 }
