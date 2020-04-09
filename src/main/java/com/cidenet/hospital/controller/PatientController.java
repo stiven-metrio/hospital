275362facc7e5eb8720f940bb4c05c7be79a9e6d
Jhonatan Sepulveda <jsepulveda@todo1.net>
2020-04-09 16:22:55 -0500

    Delete Patient is added

--- src/main/java/com/cidenet/hospital/controller/PatientController.java
+++ src/main/java/com/cidenet/hospital/controller/PatientController.java
@@ -1,11 +1,15 @@
 package com.cidenet.hospital.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,5 +51,29 @@ public class PatientController {
 	@PostMapping("/patient")
 	public Patient createPatient(@Valid @RequestBody Patient patient) {
 		return patientRepository.save(patient);
+	}
+	
+	/**
+	 * Deletes a specific patient
+	 * @param patientId
+	 * @return action status
+	 */
+	@DeleteMapping("/patient/{id}")
+	public Map<String, Boolean> deletePatient(@PathVariable(value = "id") Long patientId) {
+		
+		try {
+			patientRepository.deleteById(patientId);
+			
+			Map<String, Boolean> response = new HashMap<>();
+			response.put("deleted", Boolean.TRUE);
+		
+			return response;
+		
+		} catch (Exception e) {
+			Map<String, Boolean> response = new HashMap<>();
+			response.put("deleted", Boolean.FALSE);
+		
+			return response;
+		}
 	}
 }
